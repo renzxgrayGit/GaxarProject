dfd4cc09e69f34cd99d159749220123d6f1880a6
renzxgrayGit <renzocabajar9@gmail.com>
2024-05-03 07:06:26 +0800

    with superQuality

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from './components/Nav';
 const App = () => (
   <main className="relative">
     <Nav />
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding">
@@ -13,7 +13,7 @@ const App = () => (
     <section className="padding">
       <SuperQuality />
     </section>
-    <section className="padding">
+    <section className="padding-x py-10">
       <Services />
     </section>
     <section className="padding">
