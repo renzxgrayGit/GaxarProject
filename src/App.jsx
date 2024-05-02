57f4a409d3a9c549771fb0985af01133e1c3a9f9
renzxgrayGit <renzocabajar9@gmail.com>
2024-05-02 09:15:46 +0800

    updated gaxar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
+import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer } from './sections';
+import Nav from './components/Nav';
+
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      Popular Products
+      <PopularProducts />
     </section>
     <section className="padding">
-      Super Quality
+      <SuperQuality />
     </section>
     <section className="padding">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      Special Offer
+      <SpecialOffer />
     </section>
     <section className="bg-pale-blue padding">
-      Customer Reviews
+      <CustomerReviews />
     </section>
     <section className="bg-blue-100 padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footer
+      <Footer />
     </section>
   </main>
 )
